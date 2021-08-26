d4bbbe0358c0c92874ce9b9f9776152403127703
seth091 <k.arenaso@uniandes.edu.co>
2021-08-25 22:07:08 -0500

    mergre main

--- App/view.py
+++ App/view.py
@@ -42,11 +42,7 @@ def printMenu():
     print("Opciones:")
     print("1- Cargar Libros")
     print("2- Cargar Tags")
-<<<<<<< HEAD
-    print("3- Cargar Books-Tags!")
-=======
     print("3- Cargar Book-Tags!!!...")
->>>>>>> main
     print("0- Salir")
 
 
